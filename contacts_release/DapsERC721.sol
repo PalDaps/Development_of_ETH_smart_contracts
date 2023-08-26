c7e23ab6e8b5074ad79609c5fcb39c020bcf6081
Daps <dashko.pavel@bk.ru>
2023-08-26 16:52:27 +0300

    Useful commands

--- contacts_release/DapsERC721.sol
+++ contacts_release/DapsERC721.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// npx hardhat test
+// npx hardhat coverage
+
 // ***********************************************************************************************************************************************
 library Strings {
     function toString(uint _value) internal pure returns(string memory) {
